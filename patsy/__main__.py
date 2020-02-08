34226cfcf84f3100c9f2817086e9c4c9a3ee7e7f
Joshua Westgard <jwestgard@mac.com>
2020-02-08 17:48:03 -0500

    reorganize and more granular control over bootstrapping steps

--- patsy/__main__.py
+++ patsy/__main__.py
@@ -1,17 +1,69 @@
 #!/user/bin/env python3
 
-import os
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+import argparse
 
-from .cli import get_args
-from .model import Batch
-from .model import Asset
-from .model import Dirlist
-from .model import Instance
-from .model import Base
-from .populate import iter_accession_records_from
+from . import version
+from .database import create_schema
+from .database import load_accessions
+from .database import load_restores
 from .utils import print_header
+
+
+def get_args():
+    """
+    Create parsers and return a namespace object
+    """
+    parser = argparse.ArgumentParser(
+        description='CLI for PATSy database'
+    )
+    parser.add_argument(
+        '-v', '--version',
+        action='version',
+        help='Print version number and exit',
+        version=version
+    )
+    parser.add_argument(
+         '-d', '--database',
+         default=':memory:',
+         action='store',
+         help='Path to db file (defaults to in-memory db)',
+    )
+
+    # Subcommand interface
+    subparsers = parser.add_subparsers(
+        dest='cmd',
+        help='sub-command help'
+        )
+
+    # create the parser for the "schema" command
+    schema_subcommand = subparsers.add_parser(
+        'schema', 
+        help='Create schema from the declarative base'
+        )
+
+    # create the parser for the "load_accessions" command
+    accessions_subcommand = subparsers.add_parser(
+        'accessions', 
+        help='Load accession records'
+        )    
+    accessions_subcommand.add_argument(
+        '-s', '--source', 
+        action='store',
+        help='Source of accessions to load'
+        )
+
+    # create the parser for the "load_restores" command
+    restores_subcommand = subparsers.add_parser(
+        'restores', 
+        help='Load restored files table'
+        )    
+    restores_subcommand.add_argument(
+        '-s', '--source', 
+        action='store',
+        help='Source of restores to load'
+        )
+
+    return parser.parse_args()
 
 
 def main():
@@ -25,75 +77,20 @@ def main():
         print(f"Using database at {args.database}...")         
     db_path = f"sqlite:///{args.database}"
 
-    # Create the mapper and session
-    print("Setting up the database session...")
-    engine = create_engine(db_path)
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    print(args)
 
-    """
-    if 'bootstrap' in args.action:
-        print("Bootstrapping the database...")
-        print("Creating the schema using the declarative base...")
-        Base.metadata.create_all(engine)
-    
-    if 'load_accessions' in args.action:
-        print("Loading the accessions file..."
-        accessions_file = "/Users/westgard/Desktop/accession_catalog.csv"
-        accessions_filter = args.batch
-    
-        # Create all batch objects
-        print("Adding batches...", end="")
-        all_batches = set()
-        for rec in iter_accession_records_from(accessions_file,
-                                               accessions_filter):
-            batchname = rec.batch
-            if batchname not in all_batches:
-                all_batches.add(batchname)
-                session.add(Batch(name=batchname))
-                session.commit()
-        batch_count = session.query(Batch).count()
-        print(f"added {batch_count} batches")
-    
-        # Create all dirlist objects
-        print("Adding dirlists...", end="")
-        all_dirlists = set()
-        for rec in iter_accession_records_from(accessions_file,
-                                               accessions_filter):
-            dirlistname = rec.sourcefile
-            batchname = rec.batch
-            if dirlistname not in all_dirlists:
-                all_dirlists.add(dirlistname)
-                r, = session.query(Batch.id).filter(
-                    Batch.name == batchname).one()
-                session.add(Dirlist(filename=dirlistname, batch_id=int(r)))
-                session.commit()
-        dirlist_count = session.query(Dirlist).count()
-        print(f"added {dirlist_count} dirlists")
+    if args.cmd == 'schema':
+        create_schema(db_path)
+    elif args.cmd == 'accessions':
+        load_accessions()
+    elif args.cmd == 'restores':
+        load_restores()
 
-        # Create asset objects
-        for rec in iter_accession_records_from(accessions_file,
-                                               accessions_filter):
-            sourcefile_id, = session.query(Dirlist.id).filter(
-                Dirlist.filename == rec.sourcefile).one()
-            asset = Asset(md5=rec.md5,
-                          timestamp=rec.timestamp,
-                          filename=rec.filename,
-                          bytes=rec.bytes,
-                          dirlist_id=sourcefile_id,
-                          dirlist_line=rec.sourceline
-                          )
-            session.add(asset)
-            session.commit()
-            added_count = session.query(Asset).count()
-            print(f"Adding assets...added {added_count} assets", end="\r")
-        
     print(f"\nActions complete!")
     if args.database == ':memory:':
         print(f"Cannot query transient DB. Use -d to specify a database file.")
     else:
         print(f"Query the bootstrapped database at {args.database}.")
-    """
 
 if __name__ == "__main__":
     main()
--- patsy/database.py
+++ patsy/database.py
@@ -3,22 +3,88 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+from .model import Batch
+from .model import Asset
+from .model import Dirlist
+from .model import Instance
+from .model import Base
 
-class Database():
+
+def iter_accession_records_from(catalog_file, batch):
     """
-    Class for managing connection to a sqlite database
+    Load the accession catalog into batch, dirlist, & asset objects
     """
+    with open(catalog_file, 'r') as handle:
+        reader = csv.reader(handle, delimiter=',')
+        for row in reader:
+            if row[0] != batch:
+                continue
+            else:
+                yield AccessionRecord(*row)
 
-    def __init__(self, path):
-        if path == ":memory:":
-            print(f"Using a transient in-memory database...")
-        else:
-            print(f"Using database at {path}...")
-        self.engine = create_engine(f'sqlite:///{path}', echo=True)
-        print("Creating the schema using the declarative base...")
-        Base = declarative_base()
-        Base.metadata.create_all(self.engine)
 
-    def session(self):
-        Session = sessionmaker(bind=self.engine)
-        return Session()
+def create_schema(db_path):
+    print("Setting up the database session...")
+    engine = create_engine(db_path)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    print("Creating the schema using the declarative base...")
+    Base.metadata.create_all(engine)
+
+
+def load_accessions():
+    print("Loading the accessions file...")
+    accessions_file = "/Users/westgard/Desktop/accession_catalog.csv"
+    accessions_filter = args.batch
+
+    # Create all batch objects
+    print("Adding batches...", end="")
+    all_batches = set()
+    for rec in iter_accession_records_from(accessions_file,
+                                           accessions_filter):
+        batchname = rec.batch
+        if batchname not in all_batches:
+            all_batches.add(batchname)
+            session.add(Batch(name=batchname))
+            session.commit()
+    batch_count = session.query(Batch).count()
+    print(f"added {batch_count} batches")
+
+    # Create all dirlist objects
+    print("Adding dirlists...", end="")
+    all_dirlists = set()
+    for rec in iter_accession_records_from(accessions_file,
+                                           accessions_filter):
+        dirlistname = rec.sourcefile
+        batchname = rec.batch
+        if dirlistname not in all_dirlists:
+            all_dirlists.add(dirlistname)
+            r, = session.query(Batch.id).filter(
+                Batch.name == batchname).one()
+            session.add(Dirlist(filename=dirlistname, batch_id=int(r)))
+            session.commit()
+    dirlist_count = session.query(Dirlist).count()
+    print(f"added {dirlist_count} dirlists")
+
+    # Create asset objects
+    for rec in iter_accession_records_from(accessions_file,
+                                           accessions_filter):
+        sourcefile_id, = session.query(Dirlist.id).filter(
+            Dirlist.filename == rec.sourcefile).one()
+        asset = Asset(md5=rec.md5,
+                      timestamp=rec.timestamp,
+                      filename=rec.filename,
+                      bytes=rec.bytes,
+                      dirlist_id=sourcefile_id,
+                      dirlist_line=rec.sourceline
+                      )
+        session.add(asset)
+        session.commit()
+        added_count = session.query(Asset).count()
+        print(f"Adding assets...added {added_count} assets", end="\r")
+
+
+def load_restores():
+    pass
+
