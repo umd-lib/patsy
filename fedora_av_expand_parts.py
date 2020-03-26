95bfeebe987114d2407f720410e79a36c21549ba
David P. Steelman <dsteelma@umd.edu>
2020-03-26 08:59:52 -0400

    LIBDC-1513. Added "fedora_av_expand_parts.py" script
    Added "fedora_av_expand_parts.py" script to expand output a new CSV
    file with each row consisting of a single part, with its corresponding
    identifier and dpExtRef.
    
    If the dpExtRef field does not have enough items, a blank entry is
    added.
    
    https://issues.umd.edu/browse/LIBDC-1513

--- fedora_av_expand_parts.py
+++ fedora_av_expand_parts.py
@@ -5,10 +5,11 @@ import unittest
 """
 Each row of the "fedora_av.csv" (or its cleaned-up equivalent) has one or
 more parts, denoted by the "hasPart" field. Each entry in the "hasPart"
-field has a corresponding entry in the "identifier" field.
+field has a corresponding entry in the "identifier" and "dpExtRef" fields.
 
 This script "expands" the rows, by creating one row per entry in the "hasPart"
-field, matched to the corresponding entry in the "identifier" field.
+field, matched to the corresponding entry in the "identifier" and "dpExtRef"
+fields.
 
 For example, the following row with two parts:
 
@@ -19,10 +20,14 @@ For example, the following row with two parts:
 is converted into two rows, one for each part, with its corresponding
 identifier:
 
-|  pid     | displayTitle                                             | hasPart  | dmDate               | isMemberOfCollection | identifier   | dpExtRef                                                                                                            |
-| -------- | -------------------------------------------------------- | -------- | -------------------- | -------------------- | ------------ | ------------------------------------------------------------------------------------------------------------------- |
-|umd:2100  | The great step forward : China women in the 20th century | umd:2101 | 2003-01-01T05:00:00Z | umd:1158             | FHgreatste02 | http://streamer.lib.umd.edu/ssdcms/i.do?u=d0c16b26570c476,http://streamer.lib.umd.edu/ssdcms/i.do?u=a74cdc7b162b40b |
-|umd:2100  | The great step forward : China women in the 20th century | umd:2102 | 2003-01-01T05:00:00Z | umd:1158             | FHgreatste01 | http://streamer.lib.umd.edu/ssdcms/i.do?u=d0c16b26570c476,http://streamer.lib.umd.edu/ssdcms/i.do?u=a74cdc7b162b40b |
+|  pid     | displayTitle                                             | hasPart  | dmDate               | isMemberOfCollection | identifier   | dpExtRef                                                  |
+| -------- | -------------------------------------------------------- | -------- | -------------------- | -------------------- | ------------ | --------------------------------------------------------- |
+|umd:2100  | The great step forward : China women in the 20th century | umd:2101 | 2003-01-01T05:00:00Z | umd:1158             | FHgreatste02 | http://streamer.lib.umd.edu/ssdcms/i.do?u=d0c16b26570c476 |
+|umd:2100  | The great step forward : China women in the 20th century | umd:2102 | 2003-01-01T05:00:00Z | umd:1158             | FHgreatste01 | http://streamer.lib.umd.edu/ssdcms/i.do?u=a74cdc7b162b40b |
+
+Note: Some "dpExtRef" fields may have fewer entries than the "hasPart" field.
+In these cases, the expanded rows will have a blank "dpExtRef" field once the
+"dpExtRef" entries run out. 
 """
 
 
@@ -31,7 +36,7 @@ def expand_parts(row):
     Expands the given row into multiple rows, if the "hasPart" field has
     mulitple comma-separated values. The new rows will have a single
     entry in the "hasPart" field, the corresponding entry in the "identifier"
-    field.
+    and "dpExtRef" fields.
 
     :param row: a Dictionary representing a row from the CSV file.
     :return:
@@ -40,17 +45,25 @@ def expand_parts(row):
 
     has_part = row["hasPart"]
     identifier = row["identifier"]
+    dpExtRef = row["dpExtRef"]
 
     parts = has_part.split(",")
     ids = identifier.split(",")
+    refs = dpExtRef.split(",")
 
     if len(parts) != len(ids):
-        raise ValueError(f"pid: {row['pid']}: has_part len: {len(parts)} not equal to identifier len: {len(ids)})")
+        raise ValueError(f"Parts mismatch for pid: {row['pid']}: has_part: {len(parts)}, identifier {len(ids)}), dpExtRef: {len(refs)}")
 
-    for part, id in zip(parts, ids):
+    # Pad out refs with blanks, if it is less than parts
+    # See PID: umd:722610
+    while len(parts) > len(refs):
+        refs.append("")
+
+    for part, id, ref in zip(parts, ids, refs):
         new_row = row.copy()
-        new_row["hasPart"] = part
-        new_row["identifier"] = id
+        new_row["hasPart"] = part.strip()
+        new_row["identifier"] = id.strip()
+        new_row["dpExtRef"] = ref.strip()
 
         expanded_rows.append(new_row)
 
@@ -92,29 +105,45 @@ if __name__ == '__main__':
 
 class FedoraAvExpandPartsTest(unittest.TestCase):
     def test_expand_parts_one_part(self):
-        row = {"pid": "123", "field1": "one", "hasPart": "part1", "identifier": "id1"}
+        row = {"pid": "123", "field1": "one", "hasPart": "part1", "identifier": "id1", "dpExtRef": "ref1"}
 
         expected_result = [
-            {"pid": "123", "field1": "one", "hasPart": "part1", "identifier": "id1"}
+            {"pid": "123", "field1": "one", "hasPart": "part1", "identifier": "id1", "dpExtRef": "ref1"}
         ]
 
         result = expand_parts(row)
         self.assertEqual(expected_result, result)
 
-    def test_unequal_parts_lengths(self):
+    def test_unequal_parts_and_identifier_lengths(self):
         # The number of items in "has_parts" is not the same as the number of
         # items in "identifier"
-        row = {"pid": "123", "field1": "one", "hasPart": "part1,part2", "identifier": "id1"}
+        row = {"pid": "123", "field1": "one", "hasPart": "part1,part2", "identifier": "id1", "dpExtRef": "ref1,ref2"}
 
         with self.assertRaises(ValueError):
             expand_parts(row)
 
-    def test_expand_parts_two_parts(self):
-        row = {"pid": "123", "field1": "one", "hasPart": "part1,part2", "identifier": "id1,id2"}
+    def test_unequal_parts_and_dpExtRef_lengths_should_pad_with_blanks(self):
+        # The number of items in "has_parts" is not the same as the number of
+        # items in "dpExtRef"
+        row = {"pid": "123", "field1": "one", "hasPart": "part1,part2", "identifier": "id1,id2", "dpExtRef": "ref1"}
 
         expected_result = [
-            {"pid": "123", "field1": "one", "hasPart": "part1", "identifier": "id1"},
-            {"pid": "123", "field1": "one", "hasPart": "part2", "identifier": "id2"}
+            {"pid": "123", "field1": "one", "hasPart": "part1", "identifier": "id1", "dpExtRef": "ref1"},
+            {"pid": "123", "field1": "one", "hasPart": "part2", "identifier": "id2", "dpExtRef": ""}
+        ]
+
+        result = expand_parts(row)
+
+        self.assertEqual(2, len(result))
+        self.assertIn(result[0], expected_result)
+        self.assertIn(result[1], expected_result)
+
+    def test_expand_parts_two_parts(self):
+        row = {"pid": "123", "field1": "one", "hasPart": "part1,part2", "identifier": "id1,id2", "dpExtRef": "ref1,ref2"}
+
+        expected_result = [
+            {"pid": "123", "field1": "one", "hasPart": "part1", "identifier": "id1", "dpExtRef": "ref1"},
+            {"pid": "123", "field1": "one", "hasPart": "part2", "identifier": "id2", "dpExtRef": "ref2"}
         ]
 
         result = expand_parts(row)
@@ -124,12 +153,12 @@ class FedoraAvExpandPartsTest(unittest.TestCase):
         self.assertIn(result[1], expected_result)
 
     def test_expand_parts_three_parts(self):
-        row = {"pid": "123", "field1": "one", "hasPart": "part1,part2,part3", "identifier": "id1,id2,id3"}
+        row = {"pid": "123", "field1": "one", "hasPart": "part1,part2,part3", "identifier": "id1,id2,id3", "dpExtRef": "ref1,ref2,ref3"}
 
         expected_result = [
-            {"pid": "123", "field1": "one", "hasPart": "part1", "identifier": "id1"},
-            {"pid": "123", "field1": "one", "hasPart": "part2", "identifier": "id2"},
-            {"pid": "123", "field1": "one", "hasPart": "part3", "identifier": "id3"}
+            {"pid": "123", "field1": "one", "hasPart": "part1", "identifier": "id1", "dpExtRef": "ref1"},
+            {"pid": "123", "field1": "one", "hasPart": "part2", "identifier": "id2", "dpExtRef": "ref2"},
+            {"pid": "123", "field1": "one", "hasPart": "part3", "identifier": "id3", "dpExtRef": "ref3"}
         ]
 
         result = expand_parts(row)
