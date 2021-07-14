baef91db79f0268260fdf874a6271fe8df87c4d5
David P. Steelman <dsteelma@umd.edu>
2021-07-14 09:47:25 -0400

    LIBITD-1940. Removed obsolete tests
    Removed tests that have become obsolete due to changes in the
    return value of the "load" command.
    
    https://issues.umd.edu/browse/LIBITD-1940

--- tests/test_load.py
+++ tests/test_load.py
@@ -14,6 +14,7 @@ class TestLoad(unittest.TestCase):
             'EXTENSION': 'extension',
             'BYTES': 'bytes',
             'MTIME': 'mtime',
+            'MODDATE': 'moddate',
             'MD5': 'md5',
             'SHA1': 'sha1',
             'SHA256': 'sha256',
@@ -67,34 +68,6 @@ class TestLoad(unittest.TestCase):
         row_dict = self.valid_row_dict.copy()
         row_dict['SHA256'] = ""
         self.assertTrue(self.load.is_row_valid(csv_line_index, row_dict))
-        self.assertEqual(0, len(self.load.results['errors']))
-
-    def test_process_csv_row__invalid_row(self):
-        csv_line_index = 2
-
-        row_dict = self.valid_row_dict.copy()
-        row_dict['BATCH'] = ""
-        self.load.process_csv_row(csv_line_index, row_dict)
-        self.mock_gateway.add.assert_not_called()
-        self.assertEqual(0, self.load.results['rows_loaded'])
-        self.assertEqual(1, len(self.load.results['errors']))
-
-    def test_process_csv_row__valid_row(self):
-        csv_line_index = 2
-
-        self.load.process_csv_row(csv_line_index, self.valid_row_dict)
-        self.mock_gateway.add.assert_called()
-        self.assertEqual(1, self.load.results['rows_loaded'])
-        self.assertEqual(0, len(self.load.results['errors']))
-
-    def test_process_csv_row__valid_row__entry_in_db(self):
-        csv_line_index = 2
-
-        self.mock_gateway.add = MagicMock(return_value=False)
-        self.load.process_csv_row(csv_line_index, self.valid_row_dict)
-        self.mock_gateway.add.assert_called()
-        self.assertEqual(0, self.load.results['rows_loaded'])
-        self.assertEqual(1, self.load.results['rows_skipped'])
         self.assertEqual(0, len(self.load.results['errors']))
 
 
