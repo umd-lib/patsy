245c6f857afafcb0c25989a6a2c2dac09065154b
Marc Andreu Grillo Aguilar <aguilarm@umd.edu>
2024-01-17 10:44:45 -0500

    LIBITD-2464: Updating version number
    https://umd-dit.atlassian.net/browse/LIBITD-2464

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='patsy',
-    version='3.0.0',
+    version='3.0.1',
     description='Command-line client for preservation asset tracking system (PATSy)',
     author='Joshua A. Westgard',
     author_email="westgard@umd.edu",
